9c425b10d0f1e1c703a68106833dbfe2d51380e7
josephlee3454 <joseph.lee3454@gmail.com>
2024-02-01 11:00:19 -0800

    adding Leetcode Fins Middle Node implmentation

---
+++ FindMidNodeLeetCodeLL.js
@@ -0,0 +1,103 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+ 
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+    }
+
+    printList() {
+        let temp = this.head;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getHead() {
+        if (this.head === null) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: " + this.head.value);
+        }
+    }
+
+    getTail() {
+        if (this.tail === null) {
+            console.log("Tail: null");
+        } else {
+            console.log("Tail: " + this.tail.value);
+        }
+    }
+
+    makeEmpty() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+ 
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+
+
+// To solve this function we will use the tortise and the hare method
+
+    
+findMiddleNode() {
+    let tortise = this.head;
+    let hare = this.head;
+    let counter = 0; 
+    while (hare !== null && hare.next !== null) {
+        tortise = tortise.next;
+        hare = hare.next.next;
+        counter++;
+        console.log("iteration: " + counter + " tortise is = " + tortise)
+    }
+    return tortise;
+}
+
+
+
+
+
+}
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+
+console.log("Original list:");
+myLinkedList.printList();
+
+const middleNode = myLinkedList.findMiddleNode();
+console.log(`\nMiddle node value: ${middleNode.value}`);
+
+// Create a new list with an even number of elements
+let myLinkedList2 = new LinkedList(1);
+myLinkedList2.push(2);
+myLinkedList2.push(3);
+myLinkedList2.push(4);
+myLinkedList2.push(5);
+myLinkedList2.push(6);
+
+console.log("\nOriginal list 2:");
+myLinkedList2.printList();
+
+const middleNode2 = myLinkedList2.findMiddleNode();
+console.log(`\nMiddle node value of list 2: ${middleNode2.value}`);
